f602bc43b55c69ec44e040d4b3920667818c42a3
Samuel Ramirez <samuel_ramirez@live.com.mx>
2017-01-11 11:38:28 -0600

    cambios por actualizacion
    se corrigieron detalles por conflictos en sincronizacion

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -33,10 +33,10 @@
 	])
 	
 	.constant('api', 'http://localhost/inventario/server/public/api/')
-	/*.constant('api', 'http://api.medicavial.mx/api/')*/
+	// .constant('api', 'http://api.medicavial.mx/api/')
 
 	.constant('publicfiles','http://localhost/inventario/server/public/exports/')
-	/*.constant('publicfiles','http://api.medicavial.mx/exports/');*/
+	// .constant('publicfiles','http://api.medicavial.mx/exports/');
 	
 
 })();
